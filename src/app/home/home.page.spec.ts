7751883a4c17ad2c1ed6a0adc4e14790d3d22fbd
ArmV17 <22040111@alumno.utc.edu.mx>
2026-03-03 13:47:46 -0600

    Cambios Navbar

--- src/app/home/home.page.spec.ts
+++ src/app/home/home.page.spec.ts
@@ -1,18 +1,51 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component } from '@angular/core';
+import { 
+  IonContent, 
+  IonIcon, 
+  IonLabel 
+} from '@ionic/angular/standalone';
+import { CommonModule } from '@angular/common';
+import { addIcons } from 'ionicons'; 
+import { 
+  home, 
+  helpCircle, 
+  map, 
+  calendar, 
+  documentText, 
+  ellipsisHorizontal 
+} from 'ionicons/icons'; 
 
-import { HomePage } from './home.page';
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+  standalone: true,
+  imports: [
+    IonContent, 
+    IonIcon, 
+    IonLabel, 
+    CommonModule
+  ],
+})
+export class HomePage {
+  
+  segmentoActivo: string = 'inicio';
 
-describe('HomePage', () => {
-  let component: HomePage;
-  let fixture: ComponentFixture<HomePage>;
+  constructor() {
+    addIcons({ 
+      home, 
+      'help-circle': helpCircle, 
+      map, 
+      calendar, 
+      'document-text': documentText, 
+      'ellipsis-horizontal': ellipsisHorizontal 
+    });
+  }
 
-  beforeEach(async () => {
-    fixture = TestBed.createComponent(HomePage);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+  seleccionar(tab: string) {
+    this.segmentoActivo = tab;
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    console.log('Sección seleccionada:', tab);
+  }
+
+}
