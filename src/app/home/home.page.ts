f01d491a2dc9ad2ff56a3f9bbc7224965967131c
ArmV17 <22040111@alumno.utc.edu.mx>
2026-03-03 16:48:39 -0600

    Cambios Navbar

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,24 +1,51 @@
 import { Component } from '@angular/core';
-import { IonContent, IonIcon, IonLabel } from '@ionic/angular/standalone';
+import { 
+  IonContent, 
+  IonIcon, 
+  IonLabel 
+} from '@ionic/angular/standalone';
 import { CommonModule } from '@angular/common';
 import { addIcons } from 'ionicons'; 
-import { home, helpCircle, map, calendar, documentText, ellipsisHorizontal } from 'ionicons/icons'; 
+import { 
+  home, 
+  helpCircle, 
+  map, 
+  calendar, 
+  documentText, 
+  ellipsisHorizontal 
+} from 'ionicons/icons'; 
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonContent, IonIcon, IonLabel, CommonModule], 
+  imports: [
+    IonContent, 
+    IonIcon, 
+    IonLabel, 
+    CommonModule
+  ],
 })
 export class HomePage {
+
   segmentoActivo: string = 'inicio';
 
   constructor() {
-    addIcons({ home, 'help-circle': helpCircle, map, calendar, 'document-text': documentText, 'ellipsis-horizontal': ellipsisHorizontal });
+    addIcons({ 
+      home, 
+      'help-circle': helpCircle, 
+      map, 
+      calendar, 
+      'document-text': documentText, 
+      'ellipsis-horizontal': ellipsisHorizontal 
+    });
   }
 
   seleccionar(tab: string) {
     this.segmentoActivo = tab;
+
+    console.log('Sección seleccionada:', tab);
   }
+
 }
