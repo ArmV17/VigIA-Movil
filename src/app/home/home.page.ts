a160761cbe22487491108d30e72077359efb81b3
ArmV17 <22040111@alumno.utc.edu.mx>
2026-03-02 20:27:18 -0600

    Importacion de componentes faltantes

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+// Importamos solo lo que realmente usamos en el HTML
+import { 
+  IonContent, 
+  IonButton, 
+  IonIcon, 
+  IonLabel 
+} from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons'; // Necesario para que los iconos funcionen
+import { home, chatbubble, map } from 'ionicons/icons'; // Importa los iconos específicos
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  standalone: true,
+  // Agregamos los componentes aquí para que Angular los reconozca
+  imports: [IonContent, IonButton, IonIcon, IonLabel],
 })
 export class HomePage {
-  constructor() {}
+  constructor() {
+    // Registramos los iconos que vamos a usar
+    addIcons({ home, chatbubble, map });
+  }
 }
