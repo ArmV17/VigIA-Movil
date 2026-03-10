af3cc26cbc254a15b1811674ecbbbb0eccc4cd05
IsaPana <22040111@alumno.utc.edu.mx>
2026-03-09 18:15:51 -0600

    Credenciales (Funcionamineto en Movil)

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,9 @@
 export const environment = {
-  production: false, // En el archivo .prod.ts ponlo en true
-  apiBaseUrl: 'https://tu-api-aqui.com', 
-  apiUrl: 'https://tu-api-aqui.com/preguntas'
+  production: true,
+  apiBaseUrl: '...',
+  apiUrl: '...',
+
+  supabaseUrl: 'https://rqkzrsokdsdstjqocdbd.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJxa3pyc29rZHNkc3RqcW9jZGJkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI3NzU2MjksImV4cCI6MjA4ODM1MTYyOX0.QlfbcM5CNANqI5wKOUxmwZGrG2NjGsPE0N4ZgygAOIA',
+  mapboxToken: 'pk.eyJ1Ijoic2FsdmFoZHotMTEiLCJhIjoiY2x3czBoYTJiMDI1OTJqb2VmZzVueG1ocCJ9.dDJweS7MAR5N2U3SF64_Xw'
 };
