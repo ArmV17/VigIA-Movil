3f76b61cee5b07c8f716a241a4b67a6d59b634f7
Josue0Cg <josueozemcg@gmail.com>
2026-04-17 16:40:39 -0600

    feat: connect mobile app to production backend backendurls

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiBaseUrl: '...',
-  apiUrl: '...',
+  apiBaseUrl: 'https://vigiautc.pythonanywhere.com/api/',
+  apiUrl: 'https://vigiautc.pythonanywhere.com/chatbot/',
 
   supabaseUrl: 'https://rqkzrsokdsdstjqocdbd.supabase.co',
   supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJxa3pyc29rZHNkc3RqcW9jZGJkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI3NzU2MjksImV4cCI6MjA4ODM1MTYyOX0.QlfbcM5CNANqI5wKOUxmwZGrG2NjGsPE0N4ZgygAOIA',
